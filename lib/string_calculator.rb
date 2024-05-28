501adb30efe4a778199dae75796ed25c7c93e8d4
tanisha <tanisha.n@justmindwork.com>
2024-05-28 15:53:44 +0530

    return error message if input is valid but negative number

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,11 +3,17 @@
 class StringCalculator
   def add(*numbers)
     result = []
+    error_messages = []
     numbers.each do |number|
       num_arr = number.scan(/-?\d+/).map(&:to_i)
       negative_numbers = num_arr.select { |n| n < 0 }
+      error_messages << "Negative numbers not allowed: #{negative_numbers}" and next if negative_numbers.any?
       result << num_arr.sum
     end
-    result.join(",")
+    unless error_messages.empty?
+      error_messages.join("\n")
+    else
+      result.join(",")
+    end
   end
 end
