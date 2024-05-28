7deb4db3d66a3e5f714e095dc7338edd5a115732
tanisha <tanisha.n@justmindwork.com>
2024-05-28 15:37:45 +0530

    rspec examples

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rspec'
+RSpec.describe StringCalculator do
+  context 'with valid input' do
+    it 'returns the sum of numbers' do
+      expect(described_class.new.add("1,2,3")).to eq("6")
+    end
+
+    it 'handles negative numbers' do
+      expect(described_class.new.add("-1,2,3,-4")).to eq("Negative numbers not allowed: [-1, -4]")
+    end
+
+    it 'handles new line delimiter' do
+      expect(described_class.new.add("1\n2\n3")).to eq("6")
+    end
+
+    it 'handles custom delimiter' do
+      expect(described_class.new.add("//;\n1;2;3")).to eq("6")
+    end
+  end
+
+  context 'with invalid input' do
+    it 'handles invalid input format' do
+      expect(described_class.new.add("1,\n2")).to eq("Invalid input: '1,\n2'")
+    end
+  end
+end
