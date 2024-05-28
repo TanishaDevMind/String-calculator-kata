7e0186538bd528e884a7457b309674c8ea02eeab
tanisha <tanisha.n@justmindwork.com>
2024-05-28 15:49:51 +0530

    covered following logic 1. with valid input, return the sum of numbers 2. with valid input, handles new line delimiter 3. with valid input, handles custom delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,5 +2,12 @@
 
 class StringCalculator
   def add(*numbers)
+    result = []
+    numbers.each do |number|
+      num_arr = number.scan(/-?\d+/).map(&:to_i)
+      negative_numbers = num_arr.select { |n| n < 0 }
+      result << num_arr.sum
+    end
+    result.join(",")
   end
 end
